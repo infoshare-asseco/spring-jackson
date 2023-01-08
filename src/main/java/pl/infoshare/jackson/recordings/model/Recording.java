b6285d5f8d3a805061745ed7d5000b631a1d3137
maciej.koziara <maciej.koziara@airhelp.com>
2023-01-08 12:20:04 +0100

    2nd exercise solution

--- src/main/java/pl/infoshare/jackson/recordings/model/Recording.java
+++ src/main/java/pl/infoshare/jackson/recordings/model/Recording.java
@@ -5,9 +5,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public interface Recording {
     @JsonIgnore
     Integer getId();
+
     @JsonIgnore
     RecordingType getType();
+
     String getTitle();
+
     Recording withId(Integer id);
 
     default RecordingId getExternalId() {
